20823e77133debcd367dc636fb01887990591479
Bryanzinn81 <bryan.iagoo@gmail.com>
2024-06-13 19:35:56 -0300

    Inicializando a conexão com comandos mysql

---
+++ A3/src/BancoDados.java
@@ -0,0 +1,55 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BancoDados extends conexao {
+    public void inserirtexto(String texto, int contadorPalavra, int contadorCaracter, int contadorLinha) {
+        String sql = "INSERT INTO CONTADOR (texto, contadorPalavra, contadorCaracter, contadorLinha) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, texto);
+            pstmt.setInt(2, contadorPalavra);
+            pstmt.setInt(3, contadorCaracter);
+            pstmt.setInt(4, contadorLinha);
+            pstmt.executeUpdate();
+            System.out.println("Texto inserido com sucesso.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Erro ao inserir texto no banco de dados: " + e.getMessage());
+        }
+    }
+
+    public void atualizartexto(int id, String texto, int contadorPalavra, int contadorCaracter, int contadorLinha) {
+        String sql = "UPDATE CONTADOR SET texto = ?, contadorPalavra = ?, contadorCaracter = ?, contadorLinha = ? WHERE idCONTADOR = ?";
+
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, texto);
+            pstmt.setInt(2, contadorPalavra);
+            pstmt.setInt(3, contadorCaracter);
+            pstmt.setInt(4, contadorLinha);
+            pstmt.setInt(5, id);
+            pstmt.executeUpdate();
+            System.out.println("Texto atualizado com sucesso.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao atualizar texto no banco de dados: " + e.getMessage());
+        }
+    }
+
+    public ResultSet gettodostextos() {
+        String sql = "SELECT * FROM CONTADOR";
+        ResultSet rs = null;
+        try {
+            Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            System.out.println("Textos recuperados com sucesso.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Erro ao recuperar textos do banco de dados: " + e.getMessage());
+        }
+        return rs;
+    }
+}
